dcf7f83785fe9205ca25eae0defbeeab56a2b49e
htam <hdfair@gmail.com>
2022-05-21 13:14:46 -0400

    updated pages

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -5,7 +5,11 @@ import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
+import { useMutation } from "@apollo/client";
+import { SAVE_BOOK } from "../utils/mutations";
+
 const SearchBooks = () => {
+  const [saveBook, { error }] = useMutation(SAVE_BOOK, { errorPolicy: "all" });
   // create state for holding returned google api data
   const [searchedBooks, setSearchedBooks] = useState([]);
   // create state for holding our search field data
@@ -65,10 +69,13 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
+      const { data } = await saveBook({
+        variables: {
+          bookData: { ...bookToSave },
+        },
+      });
+      if (!data.ok) {
+        throw new Error("something went wrong!");
       }
 
       // if book successfully saves to user's account, save book id to state
